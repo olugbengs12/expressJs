908fd18e43c1873e4f5cf451acf03489e9a89f7f
Gbenga <gbengulo005@gmail.com>
2020-04-22 18:51:16 +0100

    created a route to get item by id after parsing and also did a status report

--- app.js
+++ app.js
@@ -1,11 +1,25 @@
 const express = require("express");
 const app = express();
 
+const courses = [
+  { id: 1, name: "biology" },
+  { id: 2, name: "maths" },
+  { id: 3, name: "physics" },
+  { id: 4, name: "english" },
+  { id: 5, name: "agric" }
+];
+
 app.get("/", (req, res) => {
   res.send("Hello World!!");
 });
 app.get("/api/courses", (req, res) => {
   res.send([1, 2, 3, 4, 5]);
+});
+
+app.get("/api/courses/:id", (req, res) => {
+  const course = courses.find(c => c.id === parseInt(req.params.id));
+  if (!course) res.status(404).send("Course not Found");
+  res.send(course);
 });
 
 const port = process.env.PORT || 3000;
