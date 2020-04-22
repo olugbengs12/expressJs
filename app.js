09898156199bb4691a341012aeafaaea697e7061
Gbenga <gbengulo005@gmail.com>
2020-04-22 16:38:11 +0100

    made a get req to port 3000

--- app.js
+++ app.js
@@ -1,16 +1,8 @@
-const http = require("http");
+const express = require("express");
+const app = express();
 
-const server = http.createServer((req, res) => {
-  if (req.url === "/") {
-    res.write("Hello World");
-    res.end();
-  }
-  if (req.url === "/api/courses") {
-    res.write(JSON.stringify([1, 2, 4]));
-    res.end();
-  }
+app.get("/", (req, res) => {
+  res.send("Hello World");
 });
 
-server.listen(3000);
-
-console.log("Listening on port 3000");
+app.listen(3000, () => console.log("Listening on port 3000"));
