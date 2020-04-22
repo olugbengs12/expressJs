33d0c565bde2ad0670bb775750e666a7f23d28b9
Gbenga <gbengulo005@gmail.com>
2020-04-22 16:47:47 +0100

    made a req to another route

--- app.js
+++ app.js
@@ -4,5 +4,8 @@ const app = express();
 app.get("/", (req, res) => {
   res.send("Hello World");
 });
+app.get("/api/courses", (req, res) => {
+  res.send([1, 2, 3, 4, 5]);
+});
 
 app.listen(3000, () => console.log("Listening on port 3000"));
