8e49e1d1725a38727f1f6900b299a6f81649b4ec
Gbenga <gbengulo005@gmail.com>
2020-04-23 21:13:00 +0100

    refactoring

--- app.js
+++ app.js
@@ -21,16 +21,14 @@ app.get("/api/courses", (req, res) => {
 
 app.get("/api/courses/:id", (req, res) => {
   const course = courses.find(c => c.id === parseInt(req.params.id));
-  if (!course) res.status(404).send("Course not Found");
+  if (!course) return res.status(404).send("Course not Found");
   res.send(course);
 });
 
 app.post("/api/courses/", (req, res) => {
   const { error } = validateCourse(req.body); //result.error..obj destructing
-  if (error) {
-    res.status(400).send(error.details[0].message);
-    return;
-  }
+  if (error) return res.status(400).send(error.details[0].message);
+
   const course = {
     id: courses.length + 1,
     name: req.body.name
@@ -41,13 +39,10 @@ app.post("/api/courses/", (req, res) => {
 
 app.put("/api/courses/:id", (req, res) => {
   const course = courses.find(c => c.id === parseInt(req.params.id));
-  if (!course) res.status(404).send("Course not Found");
+  if (!course) return res.status(404).send("Course not Found");
 
   const { error } = validateCourse(req.body); //result.error..obj destructing
-  if (error) {
-    res.status(400).send(error.details[0].message);
-    return;
-  }
+  if (error) return res.status(400).send(error.details[0].message);
   course.name = req.body.name;
   res.send(course);
 });
@@ -60,7 +55,7 @@ function validateCourse(course) {
 
 app.delete("/api/courses/:id", (req, res) => {
   const course = courses.find(c => c.id === parseInt(req.params.id));
-  if (!course) res.status(404).send("Course not Found");
+  if (!course) return res.status(404).send("Course not Found");
 
   //Delete
   const index = courses.indexOf(course);
